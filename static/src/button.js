f86472a5168e452661a1b9304fe6a14b8cfe4a2e
Spencer Davis <spencetd@usc.edu>
2021-03-01 21:36:54 -0800

    Functional webapp with python script backend

---
+++ static/src/button.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './button.css'
+
+class Button extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            value: "Click me to flip a python coin!"
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event)
+    {
+        event.preventDefault();
+        fetch('http://127.0.0.1:5000/function', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+              },
+            })
+              .then(res => res.json())
+              .then(res => this.setState(
+                  {
+                      value: res.value
+                  }
+              ));
+    }
+    render()
+    {
+        return(
+            <button className="button" type="button" onClick={this.handleSubmit}>{this.state.value}</button>
+        )
+    }
+    
+}
+
+export default Button;
