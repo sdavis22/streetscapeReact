a132d1e92d749e086aceec323f61860e6af303f6
Spencer Davis <spencetd@usc.edu>
2021-03-15 20:40:30 -0700

    Delete src directory

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React, { createRef, useState, useCallback } from "react";
-import "./styles.css";
-import { Image as KonvaImage, Layer, Stage, Transformer } from "react-konva";
-import useImage from "use-image";
-import { IndividualSticker } from "./individualSticker";
-import { stickersData } from "./stickers.data";
