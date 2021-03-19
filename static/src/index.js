baeb11704a5c52700f5714a8383fc0701eb33b90
Spencer Davis <spencetd@usc.edu>
2021-03-19 12:46:41 -0700

    Drag and drop working

--- static/src/index.js
+++ static/src/index.js
@@ -80,7 +80,6 @@ const App = () => {
   const [selectedID, selectShape] = React.useState(null);
   let backgroundName = '';
 
-  var count = 0;
   const checkDeselect = (e) => {
     // deselect when clicked on empty area
     const clickedOnEmpty = e.target === e.target.getStage();
@@ -94,26 +93,6 @@ const App = () => {
     dragUrl.current = e.target.src;
   }
 
-  const handleDropCanvas = (e) =>
-  {
-    e.preventDefault();
-    // register event position
-    stageRef.current.setPointersPositions(e);
-    // add image
-    images = images.concat([
-        {
-        ...stageRef.current.getPointerPosition(),
-        src: dragUrl.current,
-        },
-    ]);
-    console.log(images);
-  }
-
-  const handleUpload = (fileName) =>
-  {
-    backgroundName = fileName;
-  }
-
   return (
     <Canvas 
     handleDrag={handleDragImg} 
@@ -123,6 +102,7 @@ const App = () => {
     selectedID={selectedID}
     selectShape={selectShape}
     stageRef={stageRef}
+    dragUrl={dragUrl}
     >
     </Canvas>
   );
