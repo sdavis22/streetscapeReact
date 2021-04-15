ad9c8939e51dac3f0d2b1fb9126a58d5620a3dec
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2021-04-14 22:29:07 -0700

    image fit inside the canvas - rect

--- static/src/backgroundLayer.js
+++ static/src/backgroundLayer.js
@@ -1,57 +1,73 @@
 import React from 'react';
-import {Layer, Rect} from 'react-konva';
+import { Layer, Rect } from 'react-konva';
 
-class BackgroundLayer extends React.Component
-{
-    constructor(props)
-    {
+class BackgroundLayer extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             backgroundImage: props.backgroundImage,
             fillPatternImage: null,
             width: 1000,
-            height: 1000
+            height: 1000,
+            scaleX: 1,
+            scaleY: 1,
+            scale: 1
         };
     }
 
-    componentDidUpdate(prevProps)
-    {
+    componentDidUpdate(prevProps) {
         if (this.props.backgroundImage !== prevProps.backgroundImage) {
             const image = new window.Image();
             image.src = this.props.backgroundImage;
-            image.height = image.naturalHeight;
-            image.width = image.naturalWidth;
-            image.space = "fill"; 
-            
-            console.log(image)
+
             image.onload = () => {
-                    this.setState({
-                        backgroundImage: this.props.backgroundImage, 
-                        fillPatternImage: image,
-                        width: this.props.width,
-                        height: this.props.height
-                    })
-                }
+                this.setState({
+                    backgroundImage: this.props.backgroundImage,
+                    fillPatternImage: image,
+                    width: this.props.width,
+                    height: this.props.height
+                })
+
+                const imageWidth = this.state.fillPatternImage?.width || this.state.width;
+                const imageHeight = this.state.fillPatternImage?.height || this.state.height;
+
+                const scaleX = this.state.width / imageWidth;
+                const scaleY = this.state.height / imageHeight;
+                const _scale = Math.max(scaleX, scaleY)
+                this.setState({
+                    scaleX: scaleX,
+                    scaleY: scaleY
+                })
+                // this.setState({
+                //     scale: _scale
+                // })
+            }
+
+
         }
     }
-    
-    render()
-    {
-        
-            return(
-                this.state.fillPatternImage && <Layer>
+
+    render() {
+
+        return (
+            this.state.fillPatternImage && <Layer>
                 <Rect
-                x={0}
-                y={0}
-                width={this.state.width}
-                height={this.state.height}
-                listening={false}
-                fillPatternImage={this.state.fillPatternImage}
+                    x={0}
+                    y={0}
+                    width={this.state.width}
+                    height={this.state.height}
+                    listening={false}
+                    fillPatternImage={this.state.fillPatternImage}
+                    // fillPatternScaleX={this.state.scale}
+                    // fillPatternScaleY={this.state.scale}
+                    fillPatternScaleX={this.state.scaleX}
+                    fillPatternScaleY={this.state.scaleY}
+                    cornerRadius={10}
                 >
                 </Rect>
             </Layer>
-            );
-        
+        );
+
     }
 
 }
